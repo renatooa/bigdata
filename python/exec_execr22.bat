f2dbf5c74b81fb6f068c60027a9e18115d649e06
Renato Alves <renatoalves.infor@gmail.com>
2016-06-20 14:14:41 -0300

    leitura de arquivos texto, simulando um diretorio de arquios distribuidos

---
+++ python/exec_execr22.bat
@@ -0,0 +1,4 @@
+cd /
+cd C:\Python27
+python C:\Users\Renato\Desktop\big\Exerc\exerc21.py
+pause
---
+++ python/exerc22.py
@@ -0,0 +1,48 @@
+import mincemeat
+import glob
+import csv
+
+text_files = glob.glob('C:\\Users\\Renato\\Desktop\\big\\Exerc\\join\\*')
+
+def file_contents(file_name):
+    f = open(file_name)
+    try:
+        return f.read()
+    finally:
+        f.close
+
+source = dict((file_name, file_contents(file_name))for file_name in text_files)
+
+def mapfn(k, v):
+    print 'map ' +k   
+    for line in v.splitlines():
+        if k == 'C:\\Users\\Renato\\Desktop\\big\\Exerc\\join\\2.2-vendas.csv':
+            yield line.split(';')[0], 'vendas' + ':'+line.split(';')[5]
+        if k == 'C:\\Users\\Renato\\Desktop\\big\\Exerc\\join\\2.2-filiais.csv':
+            yield line.split(';')[0], 'filial' + ':'+line.split(';')[1]
+
+def reducefn(k, v):
+    print 'reduce ' + k
+    total = 0
+    for index, item in enumerate(v):
+        if item.split(":")[0] == 'vendas':
+            total = int(item.split(":")[1]) +total
+        if item.split(":")[0] == 'filial':
+            nomeFilial = item.split(":")[1]
+    L = list()
+    L.append(nomeFilial + " , " + str(total))
+    return L
+
+s = mincemeat.Server()
+
+s.datasource = source
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password="changeme")
+
+w = csv.writer(open("C:\\Users\\Renato\\Desktop\\big\\Exerc\\result_22.csv", "w"))
+for k, v in results.items():
+    w.writerow([k, str(v).replace("[","").replace("]","").replace("'","").replace(' ', '')])
+
+
